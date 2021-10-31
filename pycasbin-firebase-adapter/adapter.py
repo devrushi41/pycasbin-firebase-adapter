ba3804beeae4a22a06ea85a3c385a31395fd9886
Eswar DevRushi <devrushi41@gmail.com>
2021-10-31 17:37:15 +0530

    added remove policies

--- pycasbin-firebase-adapter/adapter.py
+++ pycasbin-firebase-adapter/adapter.py
@@ -10,7 +10,7 @@ class CasbinRule(object):
     CasbinRule model
     '''
 
-    def __init__(self, ptype, v0, v1, v2, v3, v4, v5):
+    def __init__(self, ptype=None, v0=None, v1=None, v2=None, v3=None, v4=None, v5=None):
         self.ptype = ptype
         self.v0 = v0
         self.v1 = v1
@@ -25,43 +25,58 @@ class CasbinRule(object):
 
     def to_dict(self):
         return {
-            'ptype': self.ptype,
-            'v0': self.v0,
-            'v1': self.v1,
-            'v2': self.v2,
-            'v3': self.v3,
-            'v4': self.v4,
-            'v5': self.v5
+            'ptype': self.ptype or None,
+            'v0': self.v0 or None,
+            'v1': self.v1 or None,
+            'v2': self.v2 or None,
+            'v3': self.v3 or None,
+            'v4': self.v4 or None,
+            'v5': self.v5 or None
         }
 
     def __str__(self):
-        return '{}, {}, {}, {}, {}, {}, {}'.format(self.ptype, self.v0, self.v1, self.v2, self.v3, self.v4, self.v5)
+        arr = [self.ptype]
+        for v in (self.v0, self.v1, self.v2, self.v3, self.v4, self.v5):
+            if v is None:
+                break
+            arr.append(v)
+        return ", ".join(arr)
 
     def __repr__(self):
         return '{} {} {} {} {} {} {}'.format(self.ptype, self.v0, self.v1, self.v2, self.v3, self.v4, self.v5)
 
 
-class Adapter(persist.Adapter):
+class Filter:
+    ptype = []
+    v0 = []
+    v1 = []
+    v2 = []
+    v3 = []
+    v4 = []
+    v5 = []
+
+
+class Adapter(persist.Adapter, persist.adapters.UpdateAdapter):
     """the interface for Casbin adapters."""
 
-    def __init__(self, firebaseCredPath=None, projectId=None):
+    def __init__(self, firebaseCredPath=None, filtered=False):
         '''
         Initialize the adapter
         '''
-        if(firebaseCredPath and projectId):
+        if(firebaseCredPath is not None):
             cred = credentials.Certificate(firebaseCredPath)
-            firebase_admin.initialize_app(cred)
         else:
             cred = credentials.ApplicationDefault()
-            firebase_admin.initialize_app(cred, {
-                'projectId': projectId,
-            })
+
+        firebase_admin.initialize_app(cred)
         self._collection = 'casbin'
         self._document = 'rules'
 
         self.db = firestore.client()
         self.rulesRef = self.db.collection(
             self._collection).document(self._document)
+
+        self._filtered = filtered
 
     def load_policy(self, model):
         '''
@@ -70,13 +85,20 @@ class Adapter(persist.Adapter):
         '''
 
         # get the rules from the collection document
-        rules = self.rulesRef.get().to_dict()['rules']
+        rulesFromDB = self.rulesRef.get().to_dict()
+        if rulesFromDB is None:
+            rules = []
+        else:
+            rules = rulesFromDB["rules"]
+
         for rule in rules:
             casbinRule = CasbinRule.from_dict(rule)
             persist.load_policy_line(str(casbinRule), model)
 
     def _save_policy_line(self, ptype, rule):
-        self.rulesRef.update({"rules": firestore.ArrayUnion([rule])})
+        casbinRule = CasbinRule(ptype, *rule)
+        self.rulesRef.update(
+            {"rules": firestore.ArrayUnion([casbinRule.to_dict()])})
 
     def save_policy(self, model):
         '''
@@ -94,9 +116,25 @@ class Adapter(persist.Adapter):
         """add policy rules to firebase"""
         self._save_policy_line(ptype, rule)
 
+    def add_policies(self, sec, ptype, rules):
+        """adds a policy rules to the storage."""
+        for rule in rules:
+            self._save_policy_line(ptype, rule)
+
+    def remove_policies(self, sec, ptype, rules):
+        """delete policy rules from firebase"""
+        ruleList = []
+        for rule in rules:
+            casbinRule = CasbinRule(ptype, *rule)
+            ruleList.append(casbinRule.to_dict())
+            self.rulesRef.update(
+                {"rules": firestore.ArrayRemove(ruleList)})
+
     def remove_policy(self, sec, ptype, rule):
         """delete policy rules from firebase"""
-        self.rulesRef.update({"rules": firestore.ArrayRemove([rule])})
+        casbinRule = CasbinRule(ptype, *rule)
+        self.rulesRef.update(
+            {"rules": firestore.ArrayRemove([casbinRule.to_dict()])})
 
     def remove_filtered_policy(self, sec, ptype, field_index, *field_values):
         """
